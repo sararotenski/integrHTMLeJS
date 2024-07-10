2f90735af82043ededb537c81ec32b11ae53ebb8
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2024-07-10 11:31:01 -0300

    Add files via upload

---
+++ MINI CALCULADORA/file.js
@@ -0,0 +1,20 @@
+let elementoInputUm = document.getElementById('inptUm')
+let elementoInputDois = document.getElementById('inptDois')
+
+function Soma(){
+
+    alert(Number(elementoInputUm.value) + Number(elementoInputDois.value))
+
+}
+
+function Subtração() {
+    alert(Number(elementoInputUm.value) - Number(elementoInputDois.value))
+}
+
+function Multiplicação() {
+    alert(Number(elementoInputUm.value) * Number(elementoInputDois.value))
+}
+
+function Divisão() {
+    alert(Number(elementoInputUm.value) / Number(elementoInputDois.value))
+}
---
+++ aula_select/file.js
@@ -0,0 +1,8 @@
+let elementoSelect = document.getElementById('selectMenu')
+let elementoInputResultado = document.getElementById('inputResultado')
+
+function MostraValorSelect(){
+   
+    elementoInputResultado.value = elementoSelect.value
+    
+}
